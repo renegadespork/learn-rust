f8b179b4f43531524546cb2a84f3b3aac2da4d28
renegadespork <jessekilburn@gmail.com>
2024-06-20 15:18:15 -0700

    Finished learning-variables project.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Learning Rust
 
-Pardon me, just learning the ropes.
+> Pardon me, just learning the ropes.
+
+This repo consists of various Rust learning projects. So far, the projects are:
+- **guessing-game**: This is a small loop that asks the user to guess a number between 1-100 and ends when they get it right, giving hints after each guess.
+- **learning-variables**: This is a small script that plays with various data types and arithmetic operations.
--- learning-variables/src/main.rs
+++ learning-variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     const PROGRAM_NAME: &str = "Learning Variables";
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
@@ -22,5 +24,37 @@ fn main() {
     println!("The value of x has returned to {x}.\n");
     println!("Three hours is {} seconds btw.\n", THREE_HOURS_IN_SECONDS);
     
-    // let 
+    {
+        let pi = 3.14159; // f64
+        let e: f32 = 2.71828; // f32
+        let quotient = pi / e;
+        let trunc_quotient = quotient as i64;
+        let wink: char = '😉';
+        println!("Also, pi / e = {}.... which is basically {}. {}\n", quotient, trunc_quotient, wink);
+    }
+    {
+        println!("Now, for something a little different.\n----------------");
+        let data1: (i32, f64, char) = (213, 78.433, '🥜');
+        let (x, y, z) = data1;
+        println!("Data: {}, {}, {}", data1.0, data1.1, data1.2);
+        println!("Data: {x}, {y}, {z}\nThese rows should match.\n")
+    }
+    {
+        let stooges = ["Moe", "Larry", "Curly"];
+        let months: [&str; 12] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let m2 = months[1];
+        println!("The Three Stooges are {}, {}, and {}.", stooges[0], stooges[1], stooges[2]);
+        println!("First two months of the year are {} and {}.\n", months[0], m2);
+    }
+    {
+        let fruits: [char; 5] = ['🍎', '🍌', '🥝', '🍓', '🍍'];
+        println!("Now for the **interactive** portion.\nPick your favorite fruit.\n1. {}\n2. {}\n3. {}\n4. {}\n5. {}", fruits[0], fruits[1], fruits[2], fruits[3], fruits[4]);
+        println!("Enter the number of your choice:\n");
+        let mut choice = String::new();
+            io::stdin()
+                .read_line(&mut choice)
+                .expect("Failed to read line.");
+        let choice: usize = choice.trim().parse().expect("Invalid choice.");
+        println!("You chose {}. Good choice!", fruits[choice-1]);
+    }
 }
