ee4ece4fe955bb4df9677d4a08bed5526c097c08
Jesse <jessekilburn@gmail.com>
2024-08-20 12:36:59 -0700

    Delete structures/target directory
    Cleaning up "target" directories. These were accidentally synced due to the gitignore file being in the wrong place.  
    
    These contain compilation data and shouldn't be present with source code.

--- structures/target/debug/deps/structures-0927bbcb279d4da0.d
+++
@@ -1,5 +0,0 @@
-/home/jesse/Github/learn-rust/structures/target/debug/deps/libstructures-0927bbcb279d4da0.rmeta: src/main.rs
-
-/home/jesse/Github/learn-rust/structures/target/debug/deps/structures-0927bbcb279d4da0.d: src/main.rs
-
-src/main.rs:
