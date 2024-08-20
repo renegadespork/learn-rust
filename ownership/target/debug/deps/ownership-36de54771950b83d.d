ebda1a4c46e22edf6239071fb96f5296e7073697
Jesse <jessekilburn@gmail.com>
2024-08-20 12:36:44 -0700

    Delete ownership/target directory
    Cleaning up "target" directories. These were accidentally synced due to the gitignore file being in the wrong place.  
    
    These contain compilation data and shouldn't be present with source code.

--- ownership/target/debug/deps/ownership-36de54771950b83d.d
+++
@@ -1,5 +0,0 @@
-/home/jesse/Github/learn-rust/ownership/target/debug/deps/libownership-36de54771950b83d.rmeta: src/main.rs
-
-/home/jesse/Github/learn-rust/ownership/target/debug/deps/ownership-36de54771950b83d.d: src/main.rs
-
-src/main.rs:
--- ownership/target/debug/deps/ownership-9c30b98d93353c70.d
+++
@@ -1,5 +0,0 @@
-/home/jesse/Github/learn-rust/ownership/target/debug/deps/libownership-9c30b98d93353c70.rmeta: src/main.rs
-
-/home/jesse/Github/learn-rust/ownership/target/debug/deps/ownership-9c30b98d93353c70.d: src/main.rs
-
-src/main.rs:
